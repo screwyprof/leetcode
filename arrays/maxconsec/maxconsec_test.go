9120051a64cd0596e65419b2e468524ddc06e814
Max O'Donnell <screwyprof@gmail.com>
2020-05-04 20:27:56 +0100

    [maxconsec] Handle empty slice

--- arrays/maxconsec/maxconsec_test.go
+++ arrays/maxconsec/maxconsec_test.go
@@ -4,7 +4,16 @@ import "testing"
 
 func TestFindMaxConsecutiveOnes(t *testing.T) {
 	t.Run("nil slice given, 0 returned", func(t *testing.T) {
+		t.Parallel()
+
 		got := findMaxConsecutiveOnes(nil)
+		assertEquals(t, 0, got)
+	})
+
+	t.Run("empty slice given, 0 returned", func(t *testing.T) {
+		t.Parallel()
+
+		got := findMaxConsecutiveOnes([]int{})
 		assertEquals(t, 0, got)
 	})
 }
