5e89e8794442be9e4f5f85e826ea2e709dc07923
Max O'Donnell <screwyprof@gmail.com>
2020-05-06 00:59:32 +0100

    [mergesort] Refactor

--- arrays/mergesort/mergesort.go
+++ arrays/mergesort/mergesort.go
@@ -8,21 +8,14 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	cnt := m + n - 1
 	m--
 	n--
-	for m >= 0 && n >= 0 {
-		if nums1[m] > nums2[n] {
+	for n >= 0 {
+		if m >= 0 && nums1[m] > nums2[n] {
 			nums1[cnt] = nums1[m]
-			cnt--
 			m--
 		} else {
 			nums1[cnt] = nums2[n]
-			cnt--
 			n--
 		}
-	}
-
-	for n >= 0 {
-		nums1[cnt] = nums2[n]
 		cnt--
-		n--
 	}
 }
