f57cae935c60155e15b85a8acabe174acd7de0ea
Max O'Donnell <screwyprof@gmail.com>
2020-05-05 17:00:40 +0100

    [dupzero] Handle two elements with zero at the beginning

--- arrays/dupzero/dupzero.go
+++ arrays/dupzero/dupzero.go
@@ -3,5 +3,16 @@ package dupzero
 // duplicateZeros duplicates each occurrence of zero, shifting the remaining elements to the right.
 // Elements beyond the length of the original array are not written.
 func duplicateZeros(arr []int) {
+	n := len(arr)
+	if n == 0 {
+		return
+	}
 
+	for i, x := range arr {
+		if x == 0 {
+			if i + 1 < n {
+				arr[i+1] = 0
+			}
+		}
+	}
 }
--- arrays/dupzero/dupzero_test.go
+++ arrays/dupzero/dupzero_test.go
@@ -18,6 +18,7 @@ func TestDuplicateZeros(t *testing.T) {
 		{"only zero elements given, nothing done", []int{0, 0, 0}, []int{0, 0, 0}},
 		{"non zero elements given, nothing done", []int{42, 27, 100}, []int{42, 27, 100}},
 		{"two elements, last zero given, nothing done", []int{42, 0}, []int{42, 0}},
+		{"two elements, first zero, second overwritten with zero", []int{0, 42}, []int{0, 0}},
 	}
 
 	for _, tc := range testCases {
