5a3412f524109afd55af059f5cff5d9b5bf0aba4
Max O'Donnell <screwyprof@gmail.com>
2020-05-06 14:17:52 +0100

    [maxconsec] Add description

--- arrays/maxconsec/maxconsec.go
+++ arrays/maxconsec/maxconsec.go
@@ -1,6 +1,16 @@
 package maxconsec
 
 // findMaxConsecutiveOnes finds the maximum number of consecutive 1s in the given array.
+// Example:
+// Input: [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s.
+// 		The maximum number of consecutive 1s is 3.
+//
+// The input array will only contain 0 and 1.
+//
+// See Max Consecutive Ones (https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3238/)
+//
 func findMaxConsecutiveOnes(nums []int) int {
 	var (
 		res int
