9c8f70a7712f60b1b8a886db1fb419a1c135caf6
Max O'Donnell <screwyprof@gmail.com>
2020-05-09 22:27:19 +0100

    [mntarr] Refactor

--- arrays/mntarr/mntarr.go
+++ arrays/mntarr/mntarr.go
@@ -1,36 +1,18 @@
 package mntarr
 
 func validMountainArray(arr []int) bool {
-	if len(arr) < 3 {
-		return false
+	n := len(arr) - 1
+
+	l := 0
+	r := n
+
+	for l < n && arr[l] < arr[l+1] {
+		l++
 	}
 
-	var peakReached bool
-	var asc bool
-	for i := 0; i < len(arr)-1; i++ {
-		if arr[i] == arr[i+1] {
-			return false
-		}
-
-		if !peakReached && arr[i] < arr[i+1] {
-			asc = true
-			continue
-		}
-
-		peakReached = true
-
-		if !asc {
-			return false
-		}
-
-		if arr[i] <= arr[i+1] {
-			return false
-		}
+	for r > 0 && arr[r-1] > arr[r] {
+		r--
 	}
 
-	if !peakReached {
-		return false
-	}
-
-	return true
+	return l > 0 && l == r && r < n
 }
