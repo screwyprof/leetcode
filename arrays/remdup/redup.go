dcb490fd56281be25327ee85bf468f4bd6a6b723
Max O'Donnell <screwyprof@gmail.com>
2020-05-09 18:08:04 +0100

    [remdup] Duplicate at the begginning given, duplicates removed, length changed

--- arrays/remdup/redup.go
+++ arrays/remdup/redup.go
@@ -1,5 +1,21 @@
 package remdup
 
 func removeDuplicates(nums []int) int {
-	return len(nums)
+	if len(nums) == 0 {
+		return 0
+	}
+
+	j := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[j] == nums[i] {
+			continue
+		}
+		j++
+		// preserve the original data
+		// in[i], in[j] = in[j], in[i]
+		// only set what is required
+		nums[j] = nums[i]
+	}
+	nums = nums[:j+1]
+	return j + 1
 }
