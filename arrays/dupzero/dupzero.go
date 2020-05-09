d06a8f8f61054723d737c4b08482d471645404e7
Max O'Donnell <screwyprof@gmail.com>
2020-05-09 23:30:28 +0100

    [dupzero] Refactor

--- arrays/dupzero/dupzero.go
+++ arrays/dupzero/dupzero.go
@@ -3,10 +3,21 @@ package dupzero
 // duplicateZeros duplicates each occurrence of zero, shifting the remaining elements to the right.
 // Elements beyond the length of the original array are not written.
 func duplicateZeros(arr []int) {
-	for i := 0; i < len(arr); i++ {
+	var i, shifts int
+	for i = 0; shifts+i < len(arr); i++ {
 		if arr[i] == 0 {
-			copy(arr[i+1:], arr[i:])
-			i++
+			shifts++
+		}
+	}
+
+	for i = i - 1; shifts > 0; i-- {
+		if i+shifts < len(arr) {
+			arr[i+shifts] = arr[i]
+		}
+
+		if arr[i] == 0 {
+			shifts--
+			arr[i+shifts] = arr[i]
 		}
 	}
 }
