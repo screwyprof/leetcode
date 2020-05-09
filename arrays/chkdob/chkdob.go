20a3ebc2406d365edded28dab7d79b016bfdc11f
Max O'Donnell <screwyprof@gmail.com>
2020-05-09 19:13:50 +0100

    [chkdob] Two elements given, the double comes first, found

--- arrays/chkdob/chkdob.go
+++ arrays/chkdob/chkdob.go
@@ -1,5 +1,15 @@
 package chkdup
 
 func checkIfExist(arr []int) bool {
+	hash := make(map[int]struct{}, len(arr))
+	for i := 0; i < len(arr); i++ {
+		hash[arr[i]] = struct{}{}
+	}
+
+	for i := range arr {
+		if _, ok := hash[2*arr[i]]; ok {
+			return true
+		}
+	}
 	return false
 }
