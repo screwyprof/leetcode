736c95bcb47b6f6ac4ef04123961a2298b0f8eb1
Max O'Donnell <screwyprof@gmail.com>
2020-05-11 22:01:48 +0100

    [repgr] Arbitrary elements given

--- arrays/repgr/repgr_test.go
+++ arrays/repgr/repgr_test.go
@@ -15,6 +15,7 @@ func TestReplaceElements(t *testing.T) {
 		{"empty slice given, nothing done", []int{}, []int{}},
 		{"one element given, [-1] returned", []int{42}, []int{-1}},
 		{"two elements given, [the max of two, -1] returned", []int{7, 42}, []int{42, -1}},
+		{"arbitrary elements given, elements replaced", []int{17, 18, 5, 4, 6, 1}, []int{18, 6, 6, 6, 1, -1}},
 	}
 
 	for _, tc := range testCases {
